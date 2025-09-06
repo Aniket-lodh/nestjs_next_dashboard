030f3f9ab1dcf83b57dc37d49baf6b5f27563bb6
Aniket-lodh <aniket.lodh.08@gmail.com>
2025-09-06 16:12:12 +0530

    updated

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', "https://*.onrender.com", "https://*.vercel.app"],
+    origin: ['http://localhost:3000', "https://*.onrender.com", "https://nestjs-next-dashboard.vercel.app"],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
